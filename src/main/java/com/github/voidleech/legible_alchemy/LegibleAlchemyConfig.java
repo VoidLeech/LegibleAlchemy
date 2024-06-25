8ce83bd3bfe4e1dc07edf37e2e1faf2b5dcbacad
VoidLeech <ian.gerritse@gmail.com>
2024-06-25 03:34:22 +0200

    chore: project setup

--- src/main/java/com/github/voidleech/legible_alchemy/LegibleAlchemyConfig.java
+++ src/main/java/com/github/voidleech/legible_alchemy/LegibleAlchemyConfig.java
@@ -1,12 +1,12 @@
-package com.github.voidleech.template;
+package com.github.voidleech.legible_alchemy;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 
-@Mod.EventBusSubscriber(modid = TemplateMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class TemplateModConfig
+@Mod.EventBusSubscriber(modid = LegibleAlchemy.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class LegibleAlchemyConfig
 {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
